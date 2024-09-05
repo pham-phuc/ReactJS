4418ed31eaba727e0f4f17064c8ca12da9da033e
vanphuc14092002@gmail.com <101715209+pham-phuc@users.noreply.github.com>
2024-09-05 15:33:02 +0700

    fist commit

--- hoc_react/src/Content.js
+++ hoc_react/src/Content.js
@@ -1,21 +1,35 @@
 import { useState, useEffect } from "react";
 
+const tabs = ["posts", "comments", "albums"];
+
 function Content() {
   const [title, settitle] = useState("");
   const [posts, setPosts] = useState([]);
+  const [type, setType] = useState("posts");
 
   //   useEffect(() => {
   //     document.title = title;
   //   });
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then((res) => res.json())
       .then((posts) => setPosts(posts));
-  }, []);
+  }, [type]);
 
   return (
     <div style={{ padding: 50 }}>
+      {tabs.map((tab) => (
+        <button
+          key="tab"
+          style={
+            type === tab ? { color: "#fff", backgroundColor: "#2ecc71" ,border:'1px solid #2ecc71'} : {}
+          }
+          onClick={() => setType(tab)}
+        >
+          {tab}
+        </button>
+      ))}
       <input value={title} onChange={(e) => settitle(e.target.value)} />
       <ul>
         {posts.map((post) => (
