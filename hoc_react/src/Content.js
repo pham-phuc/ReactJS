23409e47e36a0eab1511c801b825d53b263b1938
vanphuc14092002@gmail.com <101715209+pham-phuc@users.noreply.github.com>
2024-09-06 15:15:48 +0700

    first commit

--- hoc_react/src/Content.js
+++ hoc_react/src/Content.js
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
 
-
 function Content() {
-  const [countDown,setCountDown] = useState(180);
+  const [countDown, setCountDown] = useState(180);
+
+  useEffect(() => {
+    const timeID = setInterval(() => {
+      setCountDown((prevState) => prevState - 1);
+    }, 1000);
+    return () => clearInterval(timeID);
+  }, []);
 
   return (
     <div style={{ padding: 50 }}>
-      
+      <h1>{countDown}</h1>
     </div>
   );
 }
