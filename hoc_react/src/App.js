71a26a931e21597c4f8ce383cdeeccdb85597a30
vanphuc14092002@gmail.com <101715209+pham-phuc@users.noreply.github.com>
2024-09-12 14:36:53 +0700

    first commit

--- hoc_react/src/App.js
+++ hoc_react/src/App.js
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import Content from "./Content";
 function App() {
-  const [show, setShow] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
   return (
     <div style={{ padding: 50 }}>
-      <button
-        onClick={() => {
-          setShow(!show);
-        }}
-      >
-        Tongle
-      </button>
-      {show && <Content />}
+      <Content />
+      <h1>{count}</h1>
+      <button onClick={increase} >Click !</button>
     </div>
   );
 }
--- hoc_react/src/Content.js
+++ hoc_react/src/Content.js
@@ -1,22 +1,11 @@
-import { useState, useEffect, useLayoutEffect, useRef } from "react";
+import { memo } from "react";
 
 function Content() {
-  const [count, setCount] = useState(60);
-  let timerId = useRef();
-  const handleStart = () => {
-    timerId.current = setInterval(() => {
-      setCount((prev) => prev - 1);
-    }, 1000);
-  };
-  const handleStop = () => {
-    clearInterval(timerId.current);
-  };
+  console.log('re-render');
   return (
     <div style={{ padding: 50 }}>
-      <h1>{count}</h1>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
+      <h2>Hello Ae F8</h2>
     </div>
   );
 }
-export default Content;
+export default memo(Content);
